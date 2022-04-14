596f478ebb97cf5bcdf50492f33dec43c22d4a33
Michelle Vava <mimivava26@gmail.com>
2022-04-14 12:40:51 -0300

    adding changes from heroku to main

--- develop/views.py
+++ develop/views.py
@@ -44,7 +44,7 @@ def CreateCheckoutSessionView(request):
             line_items_list.append(
                 {"price": i.product.stripe_price_id, "quantity": i.quantity}
             )
-        YOUR_DOMAIN = "https://shareeats-app.herokuapp.com"  # change in production #changes to 8000
+        YOUR_DOMAIN = " http://127.0.0.1:8000"  # https://shareeats-app.herokuapp.com  # change in production #changes to 8000
         checkout_session = stripe.checkout.Session.create(
             payment_method_types=["card"],
             line_items=line_items_list,
